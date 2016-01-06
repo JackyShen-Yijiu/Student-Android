b90f57fb7b4673f88ad4945271d9b2b0d0ca3810
gt314995447 <18612179447@163.com>
2016-01-06 15:20:02 +0800

    解决系统改变字体大小的时候导致的界面布局混乱的问题

--- BlackCatApp/src/com/sft/vo/SuccessVO.java
+++ BlackCatApp/src/com/sft/vo/SuccessVO.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 
 public class SuccessVO implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	public String scanauditurl;
 	public String applynotes;
 	public String name;
