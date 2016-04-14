5c4e4c9c1dc88ef04cd4f63c8da9e718476956b5
gt314995447 <18612179447@163.com>
2016-04-14 15:14:14 +0800

    章节目录

--- BlackCatApp/src/com/sft/blackcatapp/RecordActivity.java
+++ BlackCatApp/src/com/sft/blackcatapp/RecordActivity.java
@@ -2,20 +2,24 @@ package com.sft.blackcatapp;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import com.jzjf.app.R;
 
-public class IntegralHelpActivity extends BaseActivity {
-	private TextView tv_code;
+public class RecordActivity extends BaseActivity {
+
+	private ListView recordlist;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		addView(R.layout.integral_help);
-		setTitleText(R.string.integra);
-		tv_code = (TextView) findViewById(R.id.tv_code);
-		tv_code.setText(app.currency);
+		addView(R.layout.record_juan);
+		setTitleText(R.string.user_record);
+		initView();
+	}
+
+	private void initView() {
+		recordlist = (ListView) findViewById(R.id.record_list);
 	}
 
 	@Override
